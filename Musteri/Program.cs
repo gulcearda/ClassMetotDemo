e3f9b6d5be1d22353774a5f5e3604a5911fc8a26
gulcearda <77541794+gulcearda@users.noreply.github.com>
2021-01-19 00:58:16 +0300

    1 ve 2. Kısım

---
+++ Musteri/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Musteri
+{
+    class Musteri
+    {
+        public string Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int HesapSayisi { get; set; }
+
+
+    }
+}
---
+++ MusteriManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MusteriManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Musteri()
+
+
+    }
+}
